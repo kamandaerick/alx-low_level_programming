4f012f54c810ceb5f6a9ca87b1e6272d0d980586
Kamanda <info.proresearchers@gmail.com>
2023-03-16 15:14:50 -0700

    Print the provided statement followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main function marking start of the program
+ *
+ * return - Returns 0 since our main function require a return of an integer
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
