0cb17c7e667078d879ac809357b7759b053144a3
Kamanda <info.proresearchers@gmail.com>
2023-03-16 15:20:42 -0700

    Print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - This is the main function marking start of the program
  *
- * return - Returns 0 since our main function require a return of an integer
+ * Return - Returns 0 since our main function require a return of an integer
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
