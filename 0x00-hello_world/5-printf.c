9bbd5266894f8b2e6e4d8e12ccbb915568823967
Kamanda <info.proresearchers@gmail.com>
2023-03-16 16:13:17 -0700

    Print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - This is the main function where execution begins
  *
  *
- * Return - The return statement returns 0 as an integer
+ * Return 0 (upon successiful execution)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
