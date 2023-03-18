b9959367221b26e8e93f7662cee86456e66ea0ff
Kamanda <info.proresearchers@gmail.com>
2023-03-17 18:48:43 -0700

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function that contains the whole program
+ *
+ * Return: This returns 0 to the int main function
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
