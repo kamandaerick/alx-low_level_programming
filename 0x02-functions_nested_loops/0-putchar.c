59c2e16d298c04732f752f3c3fb295f02bf4e087
Kamanda <info.proresearchers@gmail.com>
2023-03-21 06:38:09 -0700

    Use user-defined function _putchar to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -  This is the main function that contains the whole program
+ * _putchar: This is function that prints the characters of a given name
+ * Return: This is a return statement that returns int 0 to the main function.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
