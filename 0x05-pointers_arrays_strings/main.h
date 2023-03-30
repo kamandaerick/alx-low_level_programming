93ea47e2403377fd5451a23fecc6974ecf46c816
Kamanda <info.proresearchers@gmail.com>
2023-03-30 05:17:00 -0700

    Print a string followed by a new line to stdout

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,20 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
 
-void reset_to_98(int *n);
 int _putchar(char c);
-int _strlen(char *s);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
+void rev_string(char *s);
+void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
 #endif
+
