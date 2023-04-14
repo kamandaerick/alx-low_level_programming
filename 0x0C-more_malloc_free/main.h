bda9999f87a547793c0bc7c65f88231f7a2e915b
Kamanda <kamandacerick@gmail.com>
2023-04-14 14:22:44 -0700

    Create an array of integers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *m, char c, unsigned int n);
+int *array_range(int min, int max);
 
 
 #endif
