97f82cea05a61b9a5a2acf28b1dd9babb2838e4d
Kamanda <kamandacerick@gmail.com>
2023-04-17 08:09:01 -0700

    m

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef Project
+Questions 0-5
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOH_H
+/**
+ * struct dog - Dog's information
+ * @name: First member (dog's name)
+ * @age: Second member (dog's age)
+ * @owner: Third member (Dog's owner)
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
