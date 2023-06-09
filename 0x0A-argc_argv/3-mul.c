6b232f14e052a5600d21fdc4dc8ba19170bebddf
Kamanda Erick <erickckamanda@gmail.com>
2023-06-09 11:21:48 -0700

    Multiply two numbers supplied as command line aruments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,8 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc < 2 || argc > 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
