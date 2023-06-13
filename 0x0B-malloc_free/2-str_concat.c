1ff70116718a5507096f3611615d3c87d1c326a6
Kamanda Erick <erickckamanda@gmail.com>
2023-06-13 07:39:29 -0700

    Concatenate two strings and return a pointer to the new string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,8 +17,10 @@ char *str_concat(char *s1, char *s2)
 	int s2_len = 0;
 	int final_length = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		s1_len++;
 	for (j = 0; s2[j] != '\0'; j++)
