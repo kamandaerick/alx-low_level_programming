b1a5f8b553625451cd8ae8fa4b1277c089007aa2
Kamanda Erick <erickckamanda@gmail.com>
2023-06-13 07:35:47 -0700

    Concatenate two strings and return a pointer to the new string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * str_concat - This fucntion concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * Return: Returns NULL if string is empty and new string s successiful
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	char *solution;
+	int j = 0;
+	int s1_len = 0;
+	int s2_len = 0;
+	int final_length = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		s1_len++;
+	for (j = 0; s2[j] != '\0'; j++)
+		s2_len++;
+	final_length = s1_len + s2_len + 1;
+	solution = malloc(sizeof(char) * (final_length));
+	if (solution == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		solution[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		solution[s1_len + j] = s2[j];
+	solution[final_length] = '\0';
+	return (solution);
+}
