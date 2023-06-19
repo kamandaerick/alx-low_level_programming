e82061f0b13af32e0faffa5f5efdfb77607913ac
Kamanda Erick <erickckamanda@gmail.com>
2023-06-18 23:22:05 -0700

    Print the name of the file followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+ #include "main.h"
+#include <stdio.h>
+/**
+ * main - This function prinst the name of the file
+ * Return: Return 0 on success
+ */
+int main()
+{
+	_putchar("%s\n", __FILE__);
+	return (0);
+}
