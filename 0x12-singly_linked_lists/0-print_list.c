7e6a5de0aeebf3103a14365024a065ae330c49ba
Erick Kamanda <erickckamanda@gmail.com>
2023-06-30 12:58:00 -0700

    Print the number of elements in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_list - This function prints elements of a list_t list
+ * @h: a pointer to the list whose elements to print
+ * Return: Returns the number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0x12. C - Singly linked lists
+
+This project involved reading, understanding and solving questions regarding singly linked lists as used in C programming. 
