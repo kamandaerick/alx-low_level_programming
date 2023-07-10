ddd300f52d428def4a481bb4b90ef8e50cdd1c60
Erick Kamanda <erickckamanda@gmail.com>
2023-07-10 13:58:14 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,23 +7,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fileDescriptor;
-	int writen;
-	int wordlength = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+
+	if (text_content != NULL)
 	{
-		for (wordlength = 0; text_content[wordlength];)
-			wordlength++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fileDescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	writen = write(fileDescriptor, text_content, wordlength);
-	if (fileDescriptor <= 0)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
-	if (writen == -1)
-		return (-1);
-	close(fileDescriptor);
+
+	close(fd);
+
 	return (1);
 }
