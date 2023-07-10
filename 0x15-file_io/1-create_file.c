5bf8194c278f696322bd4834e004ef40abcb887a
Erick Kamanda <erickckamanda@gmail.com>
2023-07-10 13:55:08 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,9 @@ int create_file(const char *filename, char *text_content)
 	}
 	fileDescriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	writen = write(fileDescriptor, text_content, wordlength);
-	if (fileDescriptor <= 0 || writen == -1)
+	if (fileDescriptor <= 0)
+		return (-1);
+	if (writen == -1)
 		return (-1);
 	close(fileDescriptor);
 	return (1);
