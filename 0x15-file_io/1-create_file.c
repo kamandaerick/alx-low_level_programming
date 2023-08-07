6d86519444e44d1ab911fd8c883ae92cd735bdcf
Erick Kamanda <erickckamanda@gmail.com>
2023-08-07 12:59:04 -0700

    SOlution to task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length])
+		for (length = 0; text_content[length];)
 			length++;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
