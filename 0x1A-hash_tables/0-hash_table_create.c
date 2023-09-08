afc1c4570c1283bdbc14712729b3aa3334bc130a
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-08 15:36:58 -0700

    Task one correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,24 +1,26 @@
 #include <stdlib.h>
 #include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: the sixe of the array of the table
+ * Return: returns a new table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+	hash_table_t *new_table;
+	unsigned long int i;
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return NULL;
-
-    new_table->size = size;
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return NULL;
-    }
-
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
-
-    return new_table;
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+	return (new_table);
 }
