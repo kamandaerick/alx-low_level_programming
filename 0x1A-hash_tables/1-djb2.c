cbdeb882c370a822e75bb7c7e445b67b7b94dcb0
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-08 16:37:59 -0700

    Solution to task 2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_djb2 - implements djb2 algorithim
+ * @str: ptr to array of unsigned chars
+ * Return: hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+	
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+	return (hash);
+}
