723dd0eae81f5b6532fb115c7e5b40d197226951
Erick Kamanda <117100000+kamandaerick@users.noreply.github.com>
2024-02-10 08:51:17 +0300

    Delete 0x1E-search_algorithms directory

--- 0x1E-search_algorithms/README.md
+++
@@ -1,3 +0,0 @@
-0x1E. C - Search Algorithms
-
-This project involves tackling questions aimed at ehancing my understanding of search algorithims. The coding exercises are solved in C Programming language.
--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include <stdlib.h>
-#include <stdio.h>
-int binary_search(int *array, size_t size, int value);
-int linear_search(int *array, size_t size, int value);
-#endif /* SEARCH_ALGOS_H */
