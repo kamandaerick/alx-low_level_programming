47c00013bfd05c4f460170bfb21568e97f0cd354
Kamanda <erickckamanda@gmail.com>
2024-05-30 04:17:40 -0700

    Handles edges of the grid properly

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,17 +5,20 @@
 def island_perimeter(grid):
     """Calculate and return the perimeter of an island"""
     perimeter = 0
+    cols = len(grid[0])
+    rows = len(grid)
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
-                if grid[i][j+1] == 1:
+                if j < cols - 1 and grid[i][j+1] == 1:
                     perimeter -= 1
-                if grid[i+1][j] == 1:
+                if i < rows - 1 and grid[i+1][j] == 1:
                     perimeter -= 1
-                if grid[i][j-1] == 1:
+                if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 1
-                if grid[i-1][j] == 1:
+                if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 1
 
     return perimeter
+
